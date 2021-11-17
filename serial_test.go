29d85b8bc8599d718e338dcc1bf86a6698ef32dd
gocurr <xigua67damn@gmail.com>
2021-11-17 10:32:15 +0800

    Update serial_test.go

--- serial_test.go
+++ serial_test.go
@@ -6,10 +6,10 @@ import (
 )
 
 func TestRanges(t *testing.T) {
-	continouse := 1
+	continuous := 1
 
 	data := []interface{}{-2, -1, 0, 1, 2, 3, 4, 5, 6, 3, 3, 2, 3, 3, 3, 3, 4}
-	ranges := Ranges(data, continouse, func(v interface{}) bool {
+	ranges := Ranges(data, continuous, func(v interface{}) bool {
 		return v.(int) > 2 && v.(int) < 5
 	})
 	fmt.Println(ranges)
