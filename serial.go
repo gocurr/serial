4b703ae74f38eb45e2d064e4f4f61df2a72e4768
jes <sample@gmail.com>
2021-11-27 10:19:22 +0800

    use reflect to cover any kind of data

--- serial.go
+++ serial.go
@@ -1,11 +1,20 @@
 package serial
 
+import "reflect"
+
+// Range represents a range with a `From-To` pair
 type Range struct {
 	From, To int
 }
 
-func Ranges(data []interface{}, continuous int, match func(interface{}) bool) (result []Range) {
+// Ranges returns Range-slice
+// via an interface{} object whose elements match `match` function
+// continuously more than continouse-times
+func Ranges(data interface{}, continuous int, match func(interface{}) bool) (result []Range) {
 	// special cases
+	if reflect.TypeOf(data).Kind() != reflect.Slice {
+		return
+	}
 	if continuous < 2 {
 		return
 	}
@@ -43,11 +52,15 @@ func Ranges(data []interface{}, continuous int, match func(interface{}) bool) (r
 	return result
 }
 
-func points(data []interface{}, match func(interface{}) bool) (result []int) {
-	for i, d := range data {
-		if match(d) {
-			result = append(result, i)
+// matchIndex returns int slice
+// whose elements in target `data` slice matches `match` function
+func points(data interface{}, match func(interface{}) bool) (ints []int) {
+	value := reflect.ValueOf(data)
+	for i := 0; i < value.Len(); i++ {
+		element := value.Index(i)
+		if match(element.Interface()) {
+			ints = append(ints, i)
 		}
 	}
-	return result
+	return
 }
